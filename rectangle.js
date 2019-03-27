0bb4859d6989bfb6c41e7c92b21ce6827a86ceb6
lijianhui1997 <1774407914@qq.com>
2019-03-27 17:07:36 +0800

    v0.2

--- rectangle.js
+++ rectangle.js
@@ -1,21 +1,17 @@
-$(function() {
+$(function(){
       var $width = $('#rectangle-width'),
            $height= $('#rectangle-height'),
            $calc= $('#rectangle-calc'),                     
             blnValid = false;
-      
           $calc.click(function() {
-                //if(!blnValid) return;
-              
-                   var width  = Number($width.val()),
-                       height = Number($height.val()),
-                       p = roundFractional(width * 2 + height * 2, 2 ),
-                       a= roundFractional(width * height, 2 );
-                      console.log(a,p);
-                     $('#rectangle-perimeter').val(p);
-                   $('#rectangle-area').val(a);
-                    
-                       
+                if(!blnValid) return;
+              var width  = Number($width.val()),
+                   height = Number($height.val()),
+                    p = roundFractional(width * 2 + height * 2, 2 ),
+                    a= roundFractional(width * height, 2 );
+                   // console.log(a,p);
+                  $('#rectangle-perimeter').val(p);
+                  $('#rectangle-area').val(a);             
         });
        $width.focusout(function() {
                 blnValid = validate('#rectangle-width');   
@@ -24,10 +20,8 @@ $(function() {
                        blnValid = validate('#rectangle-height');    
            });
       function roundFractional(x, n) {
-            return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-              
+            return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);  
       }
-
         /**
          *    * 对字段进行数据合法性校验
          *       *
@@ -35,17 +29,15 @@ $(function() {
          *             * @returns boolean 验证通过为 true，验证不通过为 false
          *                */
       function validate(field) {
-            var $data    = $(field),
+            var $data = $(field),
                     $message = $(field + '-validate'),
-                            label    = $(field).attr('data-label');
+                    label = $(field).attr('data-label');
 
             if($data.val() === '') {
                     $message.html(label + '不能为空！');
-                          $data.select();
-                                return false;
+                     $data.select();
+                    return false;
                                     
             }
       }
-
-
-})
+});
